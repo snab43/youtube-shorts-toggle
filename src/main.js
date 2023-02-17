9bc48a6b5585b28b8cc5ef444849e39aaad98220
S. Naber <94073283+snab43@users.noreply.github.com>
2023-02-16 21:17:55 -0500

    Squashed commit of the following:
    commit 1eacf8dc230d319c6623c477adbfb999cd47786f
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 21:08:18 2023 -0500
    
        Removed unused icon
    
    commit 3da6cafff804ae105917bc15b031c9101b3a14db
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 21:08:02 2023 -0500
    
        Removed ability to toggle off both
    
    commit 0fbba6d4b55ca253c6ea58f6a2cd7b7bd09389e4
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 20:35:43 2023 -0500
    
        Removed SVG, simplified code
    
        Removed SVGs as they were causing issues, replaced with just text. Simplified redundant code. CSS changes.
    
    commit dddad535d270f1a83bd13ec66cbc306a0353f97a
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 19:11:41 2023 -0500
    
        Rewrote UI generator
    
        innerHTML was throwing errors with Firefox's validator. So it's rewritten
    
    commit 786968b712bed7d288dc71aca264f68bbe187df0
    Merge: 50dfd12 476cbf3
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 18:57:21 2023 -0500
    
        Merge branch 'main' into develop
    
    commit 50dfd124aec7c3421af5965834ab3161e7082cf4
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 18:48:19 2023 -0500
    
        Update README.md
    
    commit 8dc6c53e4bdb72a436eba8afc0bcad89c220d714
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 18:39:37 2023 -0500
    
        Functionality added
    
        Functionality added for toggling shorts or videos.
    
    commit af5b4fde3c5d6de045558887fcc1c16e6c69ac35
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 18:14:05 2023 -0500
    
        Added a basic UI setup
    
        Coded a functional UI that generates when a certain id is loaded.
    
    commit a4fcdb7dfb968d4eb1efc9870496c2faec4baa08
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 16:53:44 2023 -0500
    
        Created Extension Popup and added icons
    
    commit 917144aafa85416dd35a66a4c21d6d82cfed6902
    Author: S. Naber <94073283+snab43@users.noreply.github.com>
    Date:   Thu Feb 16 16:14:48 2023 -0500
    
        Initial commit

--- src/main.js
+++ src/main.js
@@ -1,25 +1,12 @@
 // Constants
-const SHORTS_ICON = '<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;" class="style-scope yt-icon"><g height="24" viewBox="0 0 24 24" width="24" class="style-scope yt-icon"><path d="M17.77 10.32c-.77-.32-1.2-.5-1.2-.5L18 9.06c1.84-.96 2.53-3.23 1.56-5.06s-3.24-2.53-5.07-1.56L6 6.94c-1.29.68-2.07 2.04-2 3.49.07 1.42.93 2.67 2.22 3.25.03.01 1.2.5 1.2.5L6 14.93c-1.83.97-2.53 3.24-1.56 5.07.97 1.83 3.24 2.53 5.07 1.56l8.5-4.5c1.29-.68 2.06-2.04 1.99-3.49-.07-1.42-.94-2.68-2.23-3.25zM10 14.65v-5.3L15 12l-5 2.65z" class="style-scope yt-icon"></path></g></svg>';
-const VIDEOS_ICON = '<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;" class="style-scope yt-icon"><g class="style-scope yt-icon"><path d="M21.58,7.19a2.51,2.51,0,0,0-1.77-1.77C18.25,5,12,5,12,5s-6.25,0-7.81.42A2.51,2.51,0,0,0,2.42,7.19,25.87,25.87,0,0,0,2,12a25.87,25.87,0,0,0,.42,4.81,2.51,2.51,0,0,0,1.77,1.77C5.75,19,12,19,12,19s6.25,0,7.81-.42a2.51,2.51,0,0,0,1.77-1.77A25.87,25.87,0,0,0,22,12,25.87,25.87,0,0,0,21.58,7.19ZM10,14.65V9.35L15,12Z" class="style-scope yt-icon"></path></g></svg>';
-
-
-// Desktop tags
 const DESKTOP_SHORTS_CONTAINERS_TAG = "ytd-grid-video-renderer";
 const DESKTOP_SHELF_TAG = "ytd-reel-shelf-renderer";
 const DESKTOP_SHORTS_TAB_SELECTOR = "ytd-guide-entry-renderer>a:not([href])";
 const DESKTOP_SHORTS_MINI_TAB_SELECTOR = "ytd-mini-guide-entry-renderer>a:not([href])";
-
-// Mobile tags
-let isMobile = location.hostname == "m.youtube.com";
-const MOBILE_SHORTS_CONTAINERS_TAG = "div[tab-identifier='FEsubscriptions']>ytm-section-list-renderer>lazy-list>ytm-item-section-renderer";
-const MOBILE_SHELF_TAG = "ytm-reel-shelf-renderer";
-const MOBILE_SHORTS_TAB_SELECTOR = "ytm-pivot-bar-item-renderer>div[class='pivot-bar-item-tab pivot-shorts']";
-
-// Desktop and mobile
 const SHELF_TAG_REGEX = /yt[dm]-reel-shelf-renderer/gm
 const SHELF_ITEM_TAG_REGEX = /yt[dm]-reel-item-renderer/gm
 
-// Variables
+// Global variables
 let shortsVisible = true;
 let videosVisible = true;
 
@@ -47,18 +34,30 @@ function waitForElement(selector) {
 
 // Setup the UI
 function setup() {
-	let titleContainer = document.getElementById("title-container");
+	let titleContainer = document.getElementById('title-container');
 
+	// Create the container for both buttons
 	let toggleContainer = document.createElement('div');
-	toggleContainer.classList.add("style-scope", "ytd-shelf-renderer");
-	toggleContainer.innerHTML = `\
-		<div class="style-scope ytd-shelf-renderer" condensed="" has-items="2">\
-			<div id="toggle-shorts-buttons-computed" class="top-level-buttons">\
-				<div id="toggle-shorts-button" class="style-scope ytd-menu-renderer toggle-button-selected" button-rendereer="">${SHORTS_ICON}</div>\
-				<div id="toggle-videos-button" class="style-scope ytd-menu-renderer toggle-button-selected" button-rendereer="">${VIDEOS_ICON}</div>\
-			</div>\
-		</div>\
-	`;
+	toggleContainer.classList.add('style-scope', 'ytd-shelf-renderer');
+	toggleContainer.setAttribute('id', 'toggleContainer');
+
+	// Create the Shorts button
+	let toggleShortsButton = document.createElement('div');
+	toggleShortsButton.classList.add('toggle-button-selected');
+	toggleShortsButton.setAttribute('id', 'toggle-shorts-button');
+	toggleShortsButton.innerText = "Shorts";
+
+	// Create the Videos button
+	let toggleVideosButton = document.createElement('div');
+	toggleVideosButton.classList.add('toggle-button-selected');
+	toggleVideosButton.setAttribute('id', 'toggle-videos-button');
+	toggleVideosButton.innerText = "Videos";
+
+	// Append the buttons to the button container
+	toggleContainer.appendChild(toggleShortsButton);
+	toggleContainer.appendChild(toggleVideosButton);
+
+	// Append to the end of the #title-container
 	titleContainer.appendChild(toggleContainer);
 
 	addListeners();
@@ -66,63 +65,54 @@ function setup() {
 
 function addListeners() {
 	waitForElement('#toggle-shorts-button').then((element) => {
-		let toggleShortsButton = document.getElementById("toggle-shorts-button");
+		let toggleShortsButton = document.getElementById('toggle-shorts-button');
 
 		toggleShortsButton.onclick = () => {
-			toggleShortsButton.classList.toggle("toggle-button-selected");
+			toggleShortsButton.classList.toggle('toggle-button-selected');
 			shortsVisible = !shortsVisible;
-			toggleShorts(shortsVisible);
+			toggleItems('href="/shorts/', shortsVisible);
+
+			if (!videosVisible) {
+				let toggleVideosButton = document.getElementById('toggle-videos-button');
+				toggleVideosButton.classList.toggle('toggle-button-selected');
+				videosVisible = !videosVisible;
+				toggleItems('href="/watch', videosVisible);
+			}
 		}
 	});
 	
 	waitForElement('#toggle-videos-button').then((element) => {
-		let toggleVideosButton = document.getElementById("toggle-videos-button");
+		let toggleVideosButton = document.getElementById('toggle-videos-button');
 
 		toggleVideosButton.onclick = () => {
-			toggleVideosButton.classList.toggle("toggle-button-selected");
+			toggleVideosButton.classList.toggle('toggle-button-selected');
 			videosVisible = !videosVisible;
-			toggleVideos(videosVisible);
+			toggleItems('href="/watch', videosVisible);
+
+			if (!shortsVisible) {
+				let toggleShortsButton = document.getElementById('toggle-shorts-button');
+				toggleShortsButton.classList.toggle('toggle-button-selected');
+				shortsVisible = !shortsVisible;
+				toggleItems('href="/shorts/', shortsVisible);
+			}
 		}
 	});
 }
 
-function toggleShorts(shortsVisible) {
-	let selectorString = isMobile ? 
-    MOBILE_SHORTS_CONTAINERS_TAG + "," + MOBILE_SHELF_TAG 
-    : DESKTOP_SHORTS_CONTAINERS_TAG + "," + DESKTOP_SHELF_TAG;
+function toggleItems(href, visibility) {
+	let selectorString = DESKTOP_SHORTS_CONTAINERS_TAG + "," + DESKTOP_SHELF_TAG;
 
 	elements = document.querySelectorAll(selectorString);
 	elements.forEach(element => {
     if ((element.tagName.toLowerCase().match(SHELF_TAG_REGEX) 
       && element.innerHTML.search(SHELF_ITEM_TAG_REGEX) != -1) 
-      || element.innerHTML.search("href=\"/shorts/") != -1)
+      || element.innerHTML.search(href) != -1)
     {
-      if (!shortsVisible) {
-        element.setAttribute("hidden", true);
+      if (!visibility) {
+        element.setAttribute('hidden', true);
       }
-      else if (element.hasAttribute("hidden")) {
-        element.removeAttribute("hidden");
-      }
-    }
-  });
-}
-
-function toggleVideos(videosVisible) {
-	let selectorString = isMobile ? 
-    MOBILE_SHORTS_CONTAINERS_TAG + "," + MOBILE_SHELF_TAG 
-    : DESKTOP_SHORTS_CONTAINERS_TAG + "," + DESKTOP_SHELF_TAG;
-
-	elements = document.querySelectorAll(selectorString);
-	elements.forEach(element => {
-    if ((element.tagName.toLowerCase().match(SHELF_TAG_REGEX) 
-      && element.innerHTML.search(SHELF_ITEM_TAG_REGEX) != -1) 
-      || element.innerHTML.search("href=\"/watch") != -1)
-    {
-      if (!videosVisible) {
-        element.setAttribute("hidden", true);
-      }
-      else if (element.hasAttribute("hidden")) {
-        element.removeAttribute("hidden");
+      else if (element.hasAttribute('hidden')) {
+        element.removeAttribute('hidden');
       }
     }
   });
